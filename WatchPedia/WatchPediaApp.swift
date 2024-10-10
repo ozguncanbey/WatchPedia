0f0069b5b3d7e01361153210f3055ffe8f5e4908
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-10-10 15:47:16 +0300

    Initial Commit

---
+++ WatchPedia/WatchPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchPediaApp.swift
+//  WatchPedia
+//
+//  Created by Özgün Can Beydili on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
