0da29547a49b98c308dc4e75e49a7e4368536878
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-12-08 17:00:34 +0300

    add time showing function in message bubble

--- WatchPedia/Views/CustomViews/ChatComponents/MessageBubble.swift
+++ WatchPedia/Views/CustomViews/ChatComponents/MessageBubble.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct MessageBubble: View {
     let message: ChatMessage
     let isCurrentUser: Bool
+    @State private var showTime = false
     
     var body: some View {
         HStack {
@@ -28,6 +29,13 @@ struct MessageBubble: View {
                         .background(message.isAdmin ? Color.yellow.opacity(0.8) : Color.blue.opacity(0.7))
                         .cornerRadius(12)
                         .foregroundColor(message.isAdmin ? .black : .white)
+                    
+                    if showTime {
+                        Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                            .padding(.leading, 25)
+                    }
                 }
             } else {
                 VStack(alignment: .leading) {
@@ -50,9 +58,19 @@ struct MessageBubble: View {
                         .background(message.isAdmin ? Color.yellow.opacity(0.8) : Color.gray.opacity(0.3))
                         .cornerRadius(12)
                         .foregroundColor(message.isAdmin ? .black : .white)
+                    
+                    if showTime {
+                        Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                            .padding(.trailing, 25)
+                    }
                 }
                 Spacer()
             }
+        }
+        .onTapGesture {
+            showTime.toggle()
         }
         .padding(.horizontal)
     }
